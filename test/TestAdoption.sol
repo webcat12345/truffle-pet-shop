36a8c49ebeb86d24c561f532e1b76db19976ae62
webcat12345 <rango0986@gmail.com>
2021-05-07 09:10:18 -0400

    test: test cases for adoption contract

---
+++ test/TestAdoption.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Adoption.sol";
+
+contract TestAdoption {
+    Adoption adoption = Adoption(DeployedAddresses.Adoption());
+
+    uint expectedPetId = 8;
+
+    address expectedAdopter = address(this);
+
+    function testUserCanAdoptPet() public {
+        uint returnedPetId = adoption.adopt(expectedPetId);
+        Assert.equal(returnedPetId, expectedPetId, "Adoption of the expected pet should match what is returned.");
+    }
+
+    function testGetAdopterAddressByPetId() public {
+        address adopter = adoption.adopters(expectedPetId);
+        Assert.equal(adopter, expectedAdopter, "Owner of the expected pet should be this contract");
+    }
+}
